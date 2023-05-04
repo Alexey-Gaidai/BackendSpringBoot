b4ffa90db5a1c21a0031ee04c2eb03aa7205ac9d
Alexey-Gaidai <1123_666@mail.ru>
2023-05-04 11:23:29 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/BackendSpringBoot/BackendSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.BackendSpringBoot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendSpringBootApplication>(*args)
+}
